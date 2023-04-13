b24558d3cea95c9fa3874e9dd03bf46182fdece8
Fredrik Hammar <fredrik.hammar@forefront.se>
2023-04-13 21:27:23 +0200

    hej

---
+++ Sweetspot/EmailService.cs
@@ -0,0 +1,43 @@
+
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace Sweetspot;
+
+public class EmailService 
+{
+    public async Task<bool> SendAsync(string email)
+    {
+        try
+        {
+            var mail = new MimeMessage();
+            mail.From.Add(new MailboxAddress("Test", "cflhammar@gmail.com"));
+            mail.Sender = new MailboxAddress("Test", "cflhammar@gmail.com");
+            mail.To.Add(MailboxAddress.Parse(email));
+            var body = new BodyBuilder();
+            mail.Subject = "Ledig tid!!";
+            body.HtmlBody = "Kolla sweetspot!";
+            mail.Body = body.ToMessageBody();
+            using var smtp = new SmtpClient();
+
+            // if (_settings.UseSSL)
+            //{
+            //   await smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.SslOnConnect);
+            // }
+            //else if (_settings.UseStartTls)
+            //{
+            await smtp.ConnectAsync("smtp-relay.sendinblue.com", 587, SecureSocketOptions.StartTls);
+            // }
+            await smtp.AuthenticateAsync("cflhammar@gmail.com", "0GHbpB1YXV73EhQv");
+            await smtp.SendAsync(mail);
+            await smtp.DisconnectAsync(true);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
--- Sweetspot/Program.cs
+++ Sweetspot/Program.cs
@@ -1,27 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
-using System.Collections.Generic;
-using System.Net.Http;
 using System.Text.Json;
 using Sweetspot;
 
-Console.WriteLine("Hello, World!");
 
-HttpClient client = new HttpClient();
+var d = "2023-04-15";
+var f = 8;
+var t = 17;
+Console.WriteLine("Letar efter starttid " + d + " > "  + f + "-" + t);
 
-var respone = await client.GetAsync("https://platform.sweetspot.io/api/tee-times?course.uuid=6ec41746-b529-46bc-ac81-514095105d54&from%5Bafter%5D=2023-04-15T00%3A00%3A00%2B02%3A00&from%5Bbefore%5D=2023-04-15T23%3A59%3A59%2B02%3A00&limit=100&order%5Bfrom%5D=asc&page=1");
-var json = respone.Content.ReadAsStringAsync().Result;
-//var json = File.ReadAllText("../../../json.txt");
 
-List<Booking>? bookings = JsonSerializer.Deserialize<List<Booking>>(json);
+    HttpClient client = new HttpClient();
+    var respone = await client.GetAsync($"https://platform.sweetspot.io/api/tee-times?course.uuid=6ec41746-b529-46bc-ac81-514095105d54&from%5Bafter%5D={d}T00%3A00%3A00%2B02%3A00&from%5Bbefore%5D={d}T23%3A59%3A59%2B02%3A00&limit=100&order%5Bfrom%5D=asc&page=1");
+    var json = respone.Content.ReadAsStringAsync().Result;
+    //var json = File.ReadAllText("../../../json.txt");
 
-bookings?.ForEach(b =>
-{
-    if (b.available_slots == 4 && 
-        b.category.name != "Stängd" &&
-        b.from.Hour < 17)
+    var found = false;
+    List<Booking>? bookings = JsonSerializer.Deserialize<List<Booking>>(json);
+
+    bookings?.ForEach(b =>
     {
-        Console.WriteLine(b.from.Date + " > " +  b.@from.Hour + ":" + b.@from.Minute);
+        if (b.available_slots == 4 && 
+            b.category.name != "Stängd" &&
+            b.from.Hour < 17)
+        {
+            Console.WriteLine(b.from.Date + " > " +  b.@from.Hour + ":" + b.@from.Minute);
+            var mailService = new EmailService();
+            mailService.SendAsync("cflhammar@gmail.com");
+            found = true;
+        }
+    });
+    if (!found)
+    {
+        var current = DateTime.Now;
+        Console.WriteLine("Ingen ledig tid just nu (" + current.Month + "/" + current.Day + " " + current.Hour + ":" + current.Minute + ")");
     }
-});
+
+
+
+
