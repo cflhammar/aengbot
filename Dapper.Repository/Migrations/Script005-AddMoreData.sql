b8b1eddbf3490500a4b5f8766a89febd2b1c9776
Fredrik Hammar <fhammar@volvocars.com>
2024-10-30 20:24:51 +0100

    add more courses

---
+++ Dapper.Repository/Migrations/Script005-AddMoreData.sql
@@ -0,0 +1,4 @@
+INSERT INTO Courses (Id, Name)
+VALUES ('f10b066a-205b-4690-af58-83c43cff55c1', 'Kyssinge'),
+       ('f10b066a-205b-4690-af58-83c43cff55c1', 'Dalen'),
+       ('2ee26a65-028d-46e0-809d-635bf3c06a5a', 'International')
